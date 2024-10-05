47af3e7e95e3fb34c26e04985032dee915b882ef
faysal-islam-fd <fahadiislam01@gmail.com>
2024-10-05 01:46:51 -0700

    ready for render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express, { urlencoded } from 'express';
 import connectMongoDB from './db/connectMongoDB.js';
-
+import path from 'path';
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js'; 
 import postRoutes from './routes/post.routes.js';
@@ -27,7 +27,7 @@ app.use(cors())
 app.use(cookieParser())
 
 
-
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/auth', authRoutes);
@@ -35,10 +35,13 @@ app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationRoutes);
 
-    
+    if(process.env.NODE_ENV === 'production'){
+        app.use(express.static(path.join(__dirname, '/frontend/dist')));
+        app.get("*",(req,res)=>{
+            res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+        })
+    }
 
-app.listen(PORT, () => {
-    
-    
+app.listen(PORT, () => {    
     connectMongoDB();
 });
