7791a86c9e4084ddadfb4a5307037eff916abf39
faysal-islam-fd <fahadiislam01@gmail.com>
2024-10-04 23:42:14 -0700

    prepare the app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express, { urlencoded } from 'express';
 import connectMongoDB from './db/connectMongoDB.js';
-
+import path from 'path';
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js'; 
 import postRoutes from './routes/post.routes.js';
@@ -16,7 +16,7 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET    
-
+    
 })
 
 const app = express();
@@ -26,12 +26,21 @@ app.use(cors())
 
 app.use(cookieParser())
 
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationRoutes);
 
-const PORT = process.env.PORT || 5000;
+    if(process.env.NODE_ENV === 'production'){
+        app.use(express.static(path.join(__dirname, '/frontend/dist')));
+        app.get("*",(req,res)=>{
+            res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+        })
+    }
 
 app.listen(PORT, () => {
     console.log(PORT);
